02aeee741e0e548d0ef0f68a5022a50be4c467f5
Muhammad Amir <92692137+Mamir21@users.noreply.github.com>
2024-05-20 21:05:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const quotes = [
   "The only way to do great work is to love what you do. – Steve Jobs",
   "Life is what happens when you're busy making other plans. – John Lennon",
-  "In the end, it's not the years in your life that count. It's the life in your years. – Abraham Lincoln",
+  "In the end, it's not the years in your life that count. It's the life in your years. – Abraham Lincoln", 
   "The greatest glory in living lies not in never falling, but in rising every time we fall. – Nelson Mandela",
   "The only impossible journey is the one you never begin. – Tony Robbins",
   "Believe you can and you're halfway there. – Theodore Roosevelt",
